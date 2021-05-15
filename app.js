6a3a727b4737bdbca946b0005bf63b62e4e262aa
Temi <tayolanre2000@yahoo.com>
2021-05-15 16:11:14 +0100

    FEAT: Added listing and selecting department

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const employee = require("./lib/employee");
 const department = require("./lib/department");
+const inquirer = require('inquirer');
 
 const addNewEmployee = (ans) => {
     console.log(ans);
 }
 
 const addNewDepartment = (ans) => {
-    console.log(ans);
-    department.createDepartment(ans);
+    department.selectDepartment(ans).then((answers) => console.log(answers));
+   // department.createDepartment(ans);
 }
 
+const showSelectedDepartment = (selected) => {
+    console.log(selected);
+}
+
+const displayDepartmentsToSelect = (ans) => {
+   department.selectDepartment(ans, showSelectedDepartment);
+}
+
+
+
 //employee.promptForDetails(addNewEmployee);
-department.promptForDetails(addNewDepartment);
+//department.promptForDetails(addNewDepartment);
+department.listAllDepartments(displayDepartmentsToSelect);
 
