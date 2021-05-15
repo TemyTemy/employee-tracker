54f22321e28cbccf73d0097afad88c45e7aa8f76
Temi <tayolanre2000@yahoo.com>
2021-05-15 18:03:05 +0100

    FEAT: Adding main menu

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const employee = require("./lib/employee");
 const department = require("./lib/department");
+const role = require("./lib/role");
 const inquirer = require('inquirer');
 
+const menuQuestions = [
+    {
+     type: 'list',
+     name: 'choice',
+     choices: ['Add Employee', 'Update Employee',
+               'Remove Employee', 'View All Employee',
+               'Exit']
+    } 
+];
+
 const addNewEmployee = (ans) => {
-    console.log(ans);
+    employee.addNewEmployee(ans);
 }
 
 const addNewDepartment = (ans) => {
@@ -19,9 +30,35 @@ const displayDepartmentsToSelect = (ans) => {
    department.selectDepartment(ans, showSelectedDepartment);
 }
 
+const displayRolesAnswers = (ans) => {
+    console.log(ans);
+}
+
 
 
 //employee.promptForDetails(addNewEmployee);
 //department.promptForDetails(addNewDepartment);
-department.listAllDepartments(displayDepartmentsToSelect);
+//department.listAllDepartments(displayDepartmentsToSelect, undefined);
+//role.promptForDetails(displayRolesAnswers);
 
+
+function presentMenu() {
+    inquirer.prompt(menuQuestions)
+    .then((answers) => {
+        executeChoice(answers.choice);
+    });
+}
+
+
+
+function executeChoice(choice) {
+    switch (choice) {
+        case 'Add Employee':
+            employee.promptForDetails(addNewEmployee);            
+            break;
+        case 'Exit':                
+            console.log('Bye');
+        default:
+            console.log('Nothing');    
+    }        
+}
